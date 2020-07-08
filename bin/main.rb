e2030cafa1e74b76cd4bd98b36130bf1a6c6e792
Ugochukwu Omeje <ugochukwuomeje1@gmail.com>
2020-07-08 07:23:57 +0100

    correct the readme file

--- bin/main.rb
+++ bin/main.rb
@@ -1,42 +1,35 @@
 require './../scrapper'
 require 'byebug'
-require "colorize"
+require 'colorize'
 
-  # This is web scrapping program the scrapes www.dupontregistry.com for the following information.
-  # www.dupontregistry.com is car ecommerce website where one can find information for expensive cars
-  # also make enquiries for the car.
-  # HOW THE PROGRAM WORKS
+# This is web scrapping program the scrapes www.dupontregistry.com for the following information.
+# www.dupontregistry.com is car ecommerce website where one can find information for expensive cars
+# also make enquiries for the car.
+# HOW THE PROGRAM WORKS
 
-  # 1 The program accepts two inputs (Make and model) and displays a list of the available cars on the site.
+# 1 The program accepts two inputs (Make and model) and displays a list of the available cars on the site.
 
-    puts '+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+puts '+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 +                                                                                                 +
 +  This is web scrapping program that gets information about all the bentley continental gt model +
 +  from www.dupontregistry.com and displays them on the console                                   +
 +                                                                                                 +
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+'.yellow
 
-    carscrapper = Scrapper.new("bentley", 'continental gt')
-    all_car_info = carscrapper.scrape
-   
-    puts '------------------------------------------------------------------------------------------------------------------------------'.red
-    puts '    Name  |      Model     |   Price   | year | Link                                                                          '.red
-    puts '------------------------------------------------------------------------------------------------------------------------------'.red
+carscrapper = Scrapper.new('bentley', 'continental gt')
+all_car_info = carscrapper.scrape
 
-    
-    all_car_info.each do |car|
+puts '-----------------------------------------------------------------------------------------------------------'.red
+puts '    Name  |   Model   |  Price  | year | Link                                                              '.red
+puts '-----------------------------------------------------------------------------------------------------------'.red
 
-      name = car[:name]
-      model = car[:model]
-      price = car[:price]
-      year = car[:year]
-      link = car[:link]
-  
-      
-      puts "| #{name} | #{model} | #{price}  | #{year} |#{link} |\n\n"
-      puts '------------------------------------------------------------------------------------------------------------------------------'
-      
-    end
- 
-  
-  
+all_car_info.each do |car|
+  name = car[:name]
+  model = car[:model]
+  price = car[:price]
+  year = car[:year]
+  link = car[:link]
+
+  puts "| #{name} | #{model} | #{price}  | #{year} |#{link} |\n\n"
+  puts '---------------------------------------------------------------------------------------------------------'
+end
